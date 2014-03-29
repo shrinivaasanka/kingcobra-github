a60a7b829f8e3c2acfa00ba13bf6b12f559d321d
ka_shrinivaasan <ka_shrinivaasan@6f359cd7-7d67-4f96-a2d6-b7df7528b215>
2014-03-29 17:12:04 +0000

    Initial implementation for KingCobra Request-Response p2p queueing mechanism described in design notes with implementation of 2 new functions: - parse_ip_address() which extracts the prepended remote client ip address - reply_to_publisher() which connects to the remote client ip parsed above and sends the message that gets queued in the remote client (and service too)
    Thus a decentralized peer-to-peer message queuing is created which might have to be persisted locally in the corresponding VIRGO cloud nodes

--- c-src/kingcobra_main_kernelspace.c
+++ c-src/kingcobra_main_kernelspace.c
@@ -63,8 +63,80 @@ EXPORT_SYMBOL(kingcobra_kernelspace_exit);
 void kingcobra_servicerequest_kernelspace(void* args)
 {
 	printk(KERN_INFO "kingcobra_servicerequest_kernelspace(): KingCobra service request received from kernel KingCobra workqueue: %s\n",(char*)args);
+	long client_ip_l=parse_ip_address((char*)args);
+	char* response=kmalloc(KCOBRA_BUF_SIZE,GFP_ATOMIC);
+	sprintf(response, "kingcobra_servicerequest_kernelspace(): REPLY:from kingcobra_servicerequest_kernelspace() to client_ip=%x",client_ip_l);
+	reply_to_publisher(client_ip_l,response);
 }
 EXPORT_SYMBOL(kingcobra_servicerequest_kernelspace);
+
+long parse_ip_address(char* request)
+{
+	char* delim="#";
+	char* token=NULL;
+	char* request_dup=kstrdup(request,GFP_ATOMIC);
+	token=strsep(&request_dup,delim);
+	printk(KERN_INFO "parse_ip_address(): token = %s\n",token);
+	unsigned long ll2;
+        kstrtoll(token,16,&ll2);
+	printk(KERN_INFO "parse_ip_address(): ll2=%d\n",ll2);
+	return ll2;
+}
+
+void reply_to_publisher(long client_ip_l, char *response)
+{
+	
+	int nr;
+	struct kvec iov;
+	struct msghdr msg;
+	int error;
+	struct socket *sock;
+	struct sockaddr_in sin;
+        int sfd, s, j;
+        size_t len;
+        ssize_t nread;
+        char buf[KCOBRA_BUF_SIZE];
+
+	/*
+	struct hostport* leastloadedhostport = get_least_loaded_hostport_from_cloud();
+        s = getaddrinfo(leastloadedhostport->host, leastloadedhostport->port, &hints, &result);
+	*/
+
+	char* hostip;
+	char* port;
+	sin.sin_family=AF_INET;
+	/*in4_pton(hostip, strlen(hostip), &sin.sin_addr.s_addr, '\0',NULL);*/
+	sin.sin_addr.s_addr=client_ip_l;
+        sin.sin_port=htons(10000);
+
+	iov.iov_base=buf;
+	iov.iov_len=sizeof(buf);	
+	msg.msg_name = (struct sockaddr *) &sin;
+	msg.msg_namelen = sizeof(struct sockaddr);
+	msg.msg_iov = (struct iovec *) &iov;
+	msg.msg_iovlen = 1;
+	msg.msg_control = NULL;
+	msg.msg_controllen = 0;
+	msg.msg_flags = 0;
+	nr=1;
+
+
+	strcpy(iov.iov_base, response);
+	error = sock_create_kern(AF_INET, SOCK_STREAM, IPPROTO_TCP, &sock);
+	printk(KERN_INFO "reply_to_publisher() syscall: created client kernel socket\n");
+	kernel_connect(sock, (struct sockaddr*)&sin, sizeof(sin) , 0);
+	printk(KERN_INFO "reply_to_publisher() syscall: connected kernel client to virgo cloudexec kernel service\n ");
+	kernel_sendmsg(sock, &msg, &iov, nr, KCOBRA_BUF_SIZE);
+	printk(KERN_INFO "reply_to_publisher() syscall: sent message: %s \n", buf);
+        len  = kernel_recvmsg(sock, &msg, &iov, nr, KCOBRA_BUF_SIZE, msg.msg_flags);
+	printk(KERN_INFO "reply_to_publisher() syscall: received message: %s \n", buf);
+        le32_to_cpus(buf);
+	printk(KERN_INFO "reply_to_publisher() syscall: le32_to_cpus(buf): %s \n", buf);
+	sock_release(sock);
+	printk(KERN_INFO "reply_to_publisher() syscall: reply_to_publisher() client socket_release() invoked\n");
+	
+	return len;
+}
 
 
 MODULE_LICENSE("GPL");
