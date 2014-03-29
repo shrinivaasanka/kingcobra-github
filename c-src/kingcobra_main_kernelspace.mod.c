b726a6f3cc17dd048e666956072c3f6d24685f2c
ka_shrinivaasan <ka_shrinivaasan@6f359cd7-7d67-4f96-a2d6-b7df7528b215>
2014-03-29 17:13:08 +0000

    Updated build generated sources and kernel object files

--- c-src/kingcobra_main_kernelspace.mod.c
+++ c-src/kingcobra_main_kernelspace.mod.c
@@ -18,12 +18,24 @@ static const struct modversion_info ____versions[]
 __used
 __attribute__((section("__versions"))) = {
 	{ 0x8a9dc593, "module_layout" },
+	{ 0xb1278c61, "kmalloc_caches" },
+	{ 0x4f84ad10, "kernel_sendmsg" },
+	{ 0x779a18af, "kstrtoll" },
+	{ 0xd3fc9f1a, "sock_release" },
 	{ 0x143054c, "filp_close" },
+	{ 0x794f5965, "sock_create_kern" },
+	{ 0x85df9b6c, "strsep" },
+	{ 0x91715312, "sprintf" },
 	{ 0xc499ae1e, "kstrdup" },
+	{ 0x8648556d, "kernel_connect" },
 	{ 0x50eedeb8, "printk" },
 	{ 0xb4390f9a, "mcount" },
 	{ 0x952664c5, "do_exit" },
+	{ 0xf0fdf6cb, "__stack_chk_fail" },
 	{ 0x68d4fad5, "call_usermodehelper_fns" },
+	{ 0xac518073, "kmem_cache_alloc_trace" },
+	{ 0xd206e501, "kernel_recvmsg" },
+	{ 0xe914e41e, "strcpy" },
 	{ 0x95a608a3, "filp_open" },
 };
 
@@ -33,4 +45,4 @@ __attribute__((section(".modinfo"))) =
 "depends=";
 
 
-MODULE_INFO(srcversion, "D1B8234EA395BC1B46AD1E3");
+MODULE_INFO(srcversion, "5820670CD7C4629FFD3D044");
