e5a4e362e3b982c0e1a8b84a60fe4589cc8cb9d3
ka_shrinivaasan <ka_shrinivaasan@6f359cd7-7d67-4f96-a2d6-b7df7528b215>
2014-08-24 15:12:24 +0000

    Updated KingCobra source and latest binaries to debug the kernel_recvmsg() kernel panic (added some more if clauses for NULL args)

--- c-src/kingcobra_main_kernelspace.mod.c
+++ c-src/kingcobra_main_kernelspace.mod.c
@@ -17,30 +17,28 @@ __attribute__((section(".gnu.linkonce.this_module"))) = {
 static const struct modversion_info ____versions[]
 __used
 __attribute__((section("__versions"))) = {
-	{ 0x1b24d8b0, __VMLINUX_SYMBOL_STR(module_layout) },
-	{ 0xabcda7ae, __VMLINUX_SYMBOL_STR(kmalloc_caches) },
+	{ 0xce128e84, __VMLINUX_SYMBOL_STR(module_layout) },
+	{ 0x4febffce, __VMLINUX_SYMBOL_STR(kmalloc_caches) },
 	{ 0x4c4fef19, __VMLINUX_SYMBOL_STR(kernel_stack) },
-	{ 0xfa8ad6e7, __VMLINUX_SYMBOL_STR(kernel_sendmsg) },
+	{ 0x11ff7079, __VMLINUX_SYMBOL_STR(kernel_sendmsg) },
 	{ 0x779a18af, __VMLINUX_SYMBOL_STR(kstrtoll) },
-	{ 0xf0d24d50, __VMLINUX_SYMBOL_STR(sock_release) },
-	{ 0x333422b, __VMLINUX_SYMBOL_STR(filp_close) },
-	{ 0x59e6046c, __VMLINUX_SYMBOL_STR(sock_create_kern) },
+	{ 0xafc5ef70, __VMLINUX_SYMBOL_STR(sock_release) },
+	{ 0xd331fcb4, __VMLINUX_SYMBOL_STR(filp_close) },
+	{ 0xc799b2af, __VMLINUX_SYMBOL_STR(sock_create_kern) },
 	{ 0x85df9b6c, __VMLINUX_SYMBOL_STR(strsep) },
 	{ 0x91715312, __VMLINUX_SYMBOL_STR(sprintf) },
 	{ 0xc499ae1e, __VMLINUX_SYMBOL_STR(kstrdup) },
-	{ 0x6f592242, __VMLINUX_SYMBOL_STR(kernel_connect) },
+	{ 0x3ad3b25d, __VMLINUX_SYMBOL_STR(kernel_connect) },
 	{ 0x50eedeb8, __VMLINUX_SYMBOL_STR(printk) },
 	{ 0xb4390f9a, __VMLINUX_SYMBOL_STR(mcount) },
 	{ 0xc5fdef94, __VMLINUX_SYMBOL_STR(call_usermodehelper) },
 	{ 0x952664c5, __VMLINUX_SYMBOL_STR(do_exit) },
-	{ 0x37c3e76, __VMLINUX_SYMBOL_STR(__gcov_merge_add) },
 	{ 0xdb7305a1, __VMLINUX_SYMBOL_STR(__stack_chk_fail) },
-	{ 0xdceb3072, __VMLINUX_SYMBOL_STR(__gcov_init) },
-	{ 0x133375cb, __VMLINUX_SYMBOL_STR(kmem_cache_alloc_trace) },
-	{ 0xd93e4b5a, __VMLINUX_SYMBOL_STR(kernel_recvmsg) },
-	{ 0xe553410a, __VMLINUX_SYMBOL_STR(vfs_write) },
+	{ 0xa4c996c2, __VMLINUX_SYMBOL_STR(kmem_cache_alloc_trace) },
+	{ 0x162062c, __VMLINUX_SYMBOL_STR(kernel_recvmsg) },
+	{ 0x3377b4b3, __VMLINUX_SYMBOL_STR(vfs_write) },
 	{ 0xe914e41e, __VMLINUX_SYMBOL_STR(strcpy) },
-	{ 0x6b7f0b64, __VMLINUX_SYMBOL_STR(filp_open) },
+	{ 0xbfc1f223, __VMLINUX_SYMBOL_STR(filp_open) },
 };
 
 static const char __module_depends[]
@@ -49,4 +47,4 @@ __attribute__((section(".modinfo"))) =
 "depends=";
 
 
-MODULE_INFO(srcversion, "B8DE28FFE00EE1C7B0ABD5A");
+MODULE_INFO(srcversion, "4B8013791042785893FA0E5");
